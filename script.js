f9b3ab38b5685cd745e5a4aac520a032e60cdffc
arqus <147430581+arqu5@users.noreply.github.com>
2023-10-25 20:05:15 -0700

    "Freelance programmer website"

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
-document.getElementById('clickMe').addEventListener('click', function() {
-    alert('Button was clicked!');
-});
+function showSection(id) {
+    const sections = document.querySelectorAll('section');
+    sections.forEach(section => {
+        if (section.id === id) {
+            section.style.display = 'block';
+        } else {
+            section.style.display = 'none';
+        }
+    });
+}
